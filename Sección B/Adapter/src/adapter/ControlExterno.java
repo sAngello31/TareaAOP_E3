14a6cd451d2653c66ba697d3062e30abbe42f48e
ZarateSofia <szarate@espol.edu.ec>
2023-06-10 02:32:09 -0500

    Convirtiendo a AspectJ

---
+++ Sección B/Adapter/src/adapter/ControlExterno.java
@@ -0,0 +1,11 @@
+package adapter;
+
+public class ControlExterno {
+	public void empezarReproduccion() {
+		System.out.println("Iniciando Reproduccion");
+	}
+	
+	public void detenerTransmision() {
+		System.out.println("Deteniendo Reproduccion");
+	}
+}
